bac7ddd2656d8a0d923799fdf007ec88e319444a
Josef Friedrich <josef@friedrich.rocks>
2023-12-30 10:42:33 +0100

    Improve script

--- resources/process-docs.mjs
+++ resources/process-docs.mjs
@@ -3,10 +3,10 @@
 import * as fs from 'node:fs'
 import * as path from 'node:path'
 
-for (const file of fs.readdirSync('./src')) {
+for (let file of fs.readdirSync('./src')) {
   console.log(file)
   file = path.join('src', file)
-  let content = fs.readFileSync(path.join('src', file), { encoding: 'utf-8' })
+  let content = fs.readFileSync(file, { encoding: 'utf-8' })
 
   content = content.replace(/(L\d+)C\d+/, '$1')
   fs.writeFileSync(file, content)
