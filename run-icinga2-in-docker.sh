6611943e5c7991dbfd12c8f494b0a53673695c41
Josef Friedrich <josef@friedrich.rocks>
2024-01-04 16:38:33 +0100

    Use docker volumes

--- run-icinga2-in-docker.sh
+++ run-icinga2-in-docker.sh
@@ -1,10 +1,14 @@
 #! /bin/sh
 
+# https://github.com/Icinga/docker-icinga2
+
+sudo docker volume create icinga-master
+
 sudo docker run \
 	--rm \
 	--name icinga-master \
+	-v icinga-master:/data \
 	-h icinga-master \
 	-p 5665:5665 \
 	-e ICINGA_MASTER=1 \
-	--volume ./resources/etc-icinga2:/data/etc/icinga2 \
 	icinga/icinga2
