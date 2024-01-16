b17ad9d1ce16a67f516283c42f39f8f9faaede4d
Josef Friedrich <josef@friedrich.rocks>
2024-01-16 07:47:16 +0100

    Add workflow to run tests using a docker container

--- run-icinga2-in-docker.sh
+++ run-icinga2-in-docker.sh
@@ -5,7 +5,7 @@
 sudo docker run \
 	--rm \
 	--name icinga-master \
-	-v ./resources/etc-icinga2:/data/etc/icinga2:rw \
+	-v ./resources/etc-icinga2:/data/etc/icinga2 \
 	-h icinga-master \
 	-p 5665:5665 \
 	-e ICINGA_MASTER=1 \
