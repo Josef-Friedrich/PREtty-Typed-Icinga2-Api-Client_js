b06de8dc282590cdd5e7d706b85d110d4aac5dab
Josef Friedrich <josef@friedrich.rocks>
2024-01-16 07:29:17 +0100

    Mount volume in docker container

--- run-icinga2-in-docker_using-volume.sh
+++ run-icinga2-in-docker_using-volume.sh
@@ -2,10 +2,12 @@
 
 # https://github.com/Icinga/docker-icinga2
 
+sudo docker volume create icinga-volume
+
 sudo docker run \
 	--rm \
 	--name icinga-master \
-	-v ./resources/etc-icinga2:/data/etc/icinga2:rw \
+	-v icinga-volume:/data \
 	-h icinga-master \
 	-p 5665:5665 \
 	-e ICINGA_MASTER=1 \
